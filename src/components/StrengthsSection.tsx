25500765f90e412f108ed20fcf7fcc9e09dd8015
sgd3970 <sgd3970@naver.com>
2025-07-13 14:23:36 +0900

    fix: StrengthsSection에서 사용하지 않는 useRef import 제거

--- src/components/StrengthsSection.tsx
+++ src/components/StrengthsSection.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { strengthsData } from '@/data/mockData';
 import { Strength } from '@/types';
 
